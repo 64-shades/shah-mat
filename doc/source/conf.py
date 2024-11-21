5753d020d586010ceff84cb99d876a5f24aa275f
John Bampton <jbampton@users.noreply.github.com>
2024-11-22 03:49:00 +1000

    Add first page (#2)

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -13,7 +13,7 @@ author = '64 Shades'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = ["myst_parser"]
 
 templates_path = ['_templates']
 exclude_patterns = []
